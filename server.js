55830f8c3093f4583140f195fba528cc6b51e7f1
dkagotho <dianakago2@gmail.com>
2020-05-06 19:11:35 -0400

    commit changes

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var bodyParser = require('body-parser');
+// var bodyParser = require('body-parser');
 // var methodOverride = require('method-override');
 var path = require('path')
 var app = express();
@@ -9,21 +9,23 @@ var PORT = process.env.PORT || 3000;
 //Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(process.cwd() + '/public'));
 
-app.use(bodyParser.urlencoded({
-	extended: false
-}));
+// app.use(bodyParser.urlencoded({
+// 	extended: false
+// }));
 app.use(express.json());
 // app.use(methodOverride('_method'));
 var exphbs = require('express-handlebars');
-app.engine('hbs', exphbs({
+app.engine('handlebars', exphbs({
     defaultLayout: 'main'
 }));
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+// app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'handlebars');
 
 var routes = require('./controllers/burgers_controller.js');
 app.use('/', routes);
 
 
-app.listen(PORT);
+app.listen(PORT, function(){
+    console.log('listening on PORT: ' + PORT) 
+});
